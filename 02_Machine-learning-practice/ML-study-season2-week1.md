dbe22ae85c64327b800f65a6413661a07658e00d
Jinhyuck Kim <gin@finger-plus.com>
2017-05-28 16:07:56 +0900

    죄송합니다

---
+++ 02_Machine-learning-practice/ML-study-season2-week1.md
@@ -0,0 +1,31 @@
+# 5/21 ML 스터디 시즌 2 1주 차
+
+## 이번 주
+- [공유한 링크 정리](https://www.one-tab.com/page/O2SNFVrgS120s3l8kTLZQQ)
+	- https://deeplearning4j.org/opendata
+	- https://medium.com/towards-data-science/simple-and-multiple-linear-regression-in-python-c928425168f9
+	- https://www.kaggle.com/uciml/datasets
+	- https://people.sc.fsu.edu/~jburkardt/datasets/regression/regression.html
+	- http://aqibsaeed.github.io/2016-07-07-TensorflowLR/
+	- https://github.com/kmather73/NotHotdog-Classifier
+	- http://yann.lecun.com/exdb/mnist/
+	- https://medium.com/@ageitgey/machine-learning-is-fun-part-3-deep-learning-and-convolutional-neural-networks-f40359318721
+- why convert image to grayscale? 보통 shape만으로 분류하기 때문에, 색상 정보는 계산을 어렵게 함. but 항상 옮은 것은 아님
+- 공부의 방향은? 
+	- preprocessing을 포함한 문제 정의 전체
+	- (v)정제되어 있는 데이터로 ML에만 집중
+
+## 앞으로 진행할 방향
+- 기본적인 ML부터 실습을 해봄 
+- 강의에 나온 실습이 아니라, 각 주제에 맞는 dataset을 구해서 구현하기
+- 이후에는 각자 관심 있는 주제로 진행하여 공유
+
+## 다음 주
+- 아래의 예시를 보고 Linear regression을 연습해본다. 
+	- 데이터셋만 보고서 직접 설계하는 것을 추천
+	- 주택과 지역에 관련된 13개의 variables로 labeled 되어있는 주택 가격을 예측
+- [보스턴 주택 가격 예측 Blog](http://aqibsaeed.github.io/2016-07-07-TensorflowLR/)
+- [Dataset](https://archive.ics.uci.edu/ml/datasets/Housing)
+- [Dataset desc 번역](https://datascienceschool.net/view-notebook/4b948c4d99c94213bf02bcff2ced88eb/)
+- [sklearn.datasets.load_boston Document](http://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_boston.html)
+- [Median Value of Owner-Occupied Housing Units](https://www.census.gov/quickfacts/meta/long_HSG495215.htm)
---
+++ 02_Machine-learning-practice/linear_regression/boston_house/brad_house.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def read_dataset(file):
+	dataset = np.genfromtxt('./housing.data', delimiter=',')
+	dataset_length = len(dataset)
+	return dataset, dataset_length
+
+def sampling_data(dataset, dataset_length):
+	rnd_indices = np.random.rand(dataset_length) < 0.80
+	training_data = [dataset[i] for i in range(dataset_length) if rnd_indices[i]]
+	validation_data = [dataset[i] for i in range(dataset_length) if not(rnd_indices[i])]
+
+	training_data = np.transpose(training_data)
+	validation_data = np.transpose(validation_data)
+	return training_data, validation_data
+
+def linear_regression(training_data, validation_data):
+	import tensorflow as tf
+	x_data = training_data[0] #CRIME
+	y_data = training_data[13]
+
+	with tf.Session() as sess:
+		W = tf.Variable(tf.random_normal([1]), name='weight')
+		b = tf.Variable(tf.random_normal([1]), name='bias')
+
+		H = W * x_data + b
+
+		cost = tf.reduce_mean(tf.square(H - y_data))
+		optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+		train = optimizer.minimize(cost)
+
+		sess.run(tf.global_variables_initializer())
+
+		print("step", "cost", "weight", "bias")
+		for step in range(5000):
+			sess.run(train)
+			print(step, sess.run(cost), sess.run(W), sess.run(b))
+		
+
+if __name__ == "__main__":
+	print("read csv file..")
+	dataset, dataset_length = read_dataset(file)
+	print("read done. Total %d records loaded." % (dataset_length,))
+
+	print("Start sampling..(0.80)")
+	training_data, validation_data = sampling_data(dataset, dataset_length)
+	print("Sampling is done.")
+
+	linear_regression(training_data, validation_data)
+
+
