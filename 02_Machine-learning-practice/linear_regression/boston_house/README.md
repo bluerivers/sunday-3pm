c6d9787fa52c518a14c277c284f78bd0b967fb9b
Jinhyuck Kim <gin@finger-plus.com>
2017-05-28 16:19:44 +0900

    과제 올림

--- 02_Machine-learning-practice/linear_regression/boston_house/README.md
+++ 02_Machine-learning-practice/linear_regression/boston_house/README.md
@@ -1,4 +1,24 @@
-http://aqibsaeed.github.io/2016-07-07-TensorflowLR/
+# Boston Housing
 
-* 샘플에 대한 각자한 구현
-- brad : brad_house.py
+https://aqibsaeed.github.io/2016-07-07-TensorflowLR/ 의 목표를 따라해보고 연습하기 위한 프로젝트입니다. 데이터에 대한 설명은 [국문](https://datascienceschool.net/view-notebook/4b948c4d99c94213bf02bcff2ced88eb/)으로 확인 가능합니다.
+
+
+    Target
+        Boston, Owner's House Price, 1978
+        506 Towns' Median Prices (unit: USD 1,000)
+    Feature
+        CRIM: 범죄율
+        INDUS: 비소매상업지역 면적 비율
+        NOX: 일산화질소 농도
+        RM: 주택당 방 수
+        LSTAT: 인구 중 하위 계층 비율
+        B: 인구 중 흑인 비율
+        PTRATIO: 학생/교사 비율
+        ZN: 25,000 평방피트를 초과 거주지역 비율
+        CHAS: 찰스강의 경계에 위치한 경우는 1, 아니면 0
+        AGE: 1940년 이전에 건축된 소유주택의 비율
+        RAD: 방사형 고속도로까지의 거리
+        DIS: 직업센터의 거리
+        TAX: 재산세율
+
+
---
+++ 02_Machine-learning-practice/linear_regression/boston_house/gin.py
@@ -0,0 +1,76 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# %matplotlib inline
+
+data = np.loadtxt('./housing.data', delimiter=',', dtype=np.float32)
+
+# 마지막 열을 label로 하고 나머지는 feature로 구분함
+# 일단 정규화는 하지 않음
+
+print(data.shape)
+features = data[:, :-1]
+labels = data[:, [-1]]
+
+print(features.shape)
+print(labels.shape)
+
+mu = np.mean(features, axis=0)
+sigma = np.std(features, axis=0)
+features = (features - mu) / sigma
+
+n_training_samples = features.shape[0]
+n_dim = features.shape[1] + 1
+
+features = np.c_[np.ones(n_training_samples), features]
+
+rnd_indices = np.random.rand(len(features)) < 0.80
+
+train_x = features[rnd_indices]
+train_y = labels[rnd_indices]
+test_x = features[~rnd_indices]
+test_y = labels[~rnd_indices]
+
+learning_rate = 0.1
+training_epochs = 1000
+cost_history = np.empty(shape=[1], dtype=float)
+
+X = tf.placeholder(tf.float32, [None, n_dim])
+Y = tf.placeholder(tf.float32, [None, 1])
+W = tf.Variable(tf.ones([n_dim, 1]))
+
+init = tf.global_variables_initializer()
+
+Y_Hat = tf.matmul(X, W)
+cost = tf.reduce_mean(tf.square(Y_Hat - Y))
+training_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+sess = tf.Session()
+sess.run(init)
+
+for epoch in range(training_epochs):
+    sess.run(training_step, feed_dict={X: train_x, Y: train_y})
+    c = sess.run(cost, feed_dict={X: train_x, Y: train_y})
+    cost_history = np.append(cost_history, c)
+    w = sess.run(W)
+    print(epoch, c, w)
+
+
+plt.plot(range(len(cost_history)), cost_history)
+plt.axis([0, training_epochs, 0, np.max(cost_history)])
+plt.show()
+
+
+pred_y = sess.run(Y_Hat, feed_dict={X: test_x})
+mse = tf.reduce_mean(tf.square(pred_y - test_y))
+print("MSE: %.4f" % sess.run(mse))
+
+fig, ax = plt.subplots()
+ax.scatter(test_y, pred_y)
+ax.plot([test_y.min(), test_y.max()], [test_y.min(), test_y.max()], 'k--', lw=3)
+ax.set_xlabel('Measured')
+ax.set_ylabel('Predicted')
+plt.show()
+
+sess.close()
