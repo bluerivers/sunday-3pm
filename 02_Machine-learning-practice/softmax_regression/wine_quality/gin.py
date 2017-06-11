8a57f95be338b8410f97f20e47d3a12e813a1017
Jinhyuck Kim <bluerivers.starkid@gmail.com>
2017-06-11 14:50:16 +0900

    코드 다듬기

--- 02_Machine-learning-practice/softmax_regression/wine_quality/gin.py
+++ 02_Machine-learning-practice/softmax_regression/wine_quality/gin.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # set up
 
 data = np.loadtxt('./winequality-red.csv', delimiter=';', dtype=np.float32)
-# data = np.r_[data, np.loadtxt('./winequality-white.csv', delimiter=';', dtype=np.float32)]
+# data = np.loadtxt('./winequality-white.csv', delimiter=';', dtype=np.float32)
 
 print(data.shape)
 features = data[:, :-1]
@@ -34,12 +34,11 @@ test_x = features[~rnd_indices]
 test_y = labels[~rnd_indices]
 
 
-X = tf.placeholder(tf.float32, [None, 11])  # x_data와 같은 크기의 열 가짐. 행 크기는 모름.
-Y = tf.placeholder(tf.float32, [None, 1])  # tf.float32라고 써도 됨
+X = tf.placeholder(tf.float32, [None, 11])
+Y = tf.placeholder(tf.float32, [None, 1])
 
-W = tf.Variable(tf.random_normal([11, 11]))       # 3x3 행렬. 전체 0.
+W = tf.Variable(tf.random_normal([11, 11]))
 
-# softmax 알고리듬 적용. X*W = (8x3) * (3x3) = (8x3)
 nb_classes = 11
 
 b = tf.Variable(tf.random_normal([nb_classes]), name='bias')
@@ -53,8 +52,6 @@ labels_encoding = tf.one_hot(train_y, nb_classes)
 labels_encoding = tf.reshape(labels_encoding, [-1, nb_classes])
 
 
-
-# cross-entropy cost 함수
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels_encoding))
 
 learning_rate = 0.01
@@ -92,7 +89,7 @@ with tf.Session() as sess:
     correct_prediction = tf.equal(prediction, tf.argmax(test_labels_encoding, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    print(sess.run(accuracy, feed_dict={X: test_x, Y: test_y}))
+    print("Test accuracy: {:.2%}".format(sess.run(accuracy, feed_dict={X: test_x, Y: test_y})))
 
 
 
